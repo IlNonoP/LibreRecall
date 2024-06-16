fffcb939cc1755bf97adeb3f27851deb8a097dfc
IlNonoP <IlNonoP@outlook.it>
2024-06-16 10:23:40 +0200

    The firt version

---
+++ take-screenshot.py
@@ -0,0 +1,24 @@
+import os
+from PIL import Image
+import pytesseract
+from datetime import datetime
+
+# Esegui lo screenshot utilizzando flameshot
+os.system("flameshot full --path static/images --delay 0")
+
+# OCR dell'immagine catturata
+image_path = 'static/images/screen.png'
+ocr_text = pytesseract.image_to_string(Image.open(image_path), lang='ita_old')
+# print(ocr_text)
+
+# Genera un nome univoco per i file
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+name_png = f"{timestamp}.png"
+name_txt = f"{timestamp}.txt"
+
+# Rinomina lo screenshot catturato
+os.system(f"mv {image_path} static/images/{name_png}")
+
+# Scrivi il testo OCR su un file di testo
+with open(f"static/images/{name_txt}", 'w') as file:
+    file.write(ocr_text)
