da1a5cfb99602103eeee1207c97cef240f019d7d
IlNonoP <IlNonoP@outlook.it>
2024-06-16 19:13:19 +0200

    Update main.py
    update to python3

--- Source/main.py
+++ Source/main.py
@@ -2,10 +2,10 @@ import subprocess
 import time
 
 def take_screenshot():
-    subprocess.Popen(['python', 'take-screenshot.py'])
+    subprocess.Popen(['python3', 'take-screenshot.py'])
 
 def run_app():
-    subprocess.Popen(['python', 'app.py'])
+    subprocess.Popen(['python3', 'app.py'])
 
 if __name__ == "__main__":
     while True:
