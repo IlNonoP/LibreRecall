f7d7fc2c5d7485c91edc33acf41d5596c44b1a51
IlNonoP <IlNonoP@outlook.it>
2024-06-17 11:14:33 +0200

    Update Gnu-linux_installer.py

--- installer/Gnu-linux_installer.py
+++ installer/Gnu-linux_installer.py
@@ -2,7 +2,7 @@ import os
 print("Select your distribution")
 print("1 = Debian/Ubuntu")
 print("2 = Arch")
-scleta = input("Write and click enter: ")
+scelta = input("Write and click enter: ")
 if scelta == 1:
     os.system("sudo apt install flameshot tesseract-ocr tesseract-ocr-eng python3-pip")
     s.system("pip install pillow pyautogui pytesseract flask")
