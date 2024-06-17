7e7c4ea657907c3f0765ae2d31828409045ac27d
IlNonoP <IlNonoP@outlook.it>
2024-06-17 11:13:17 +0200

    Create Gnu-linux_installer.py

---
+++ installer/Gnu-linux_installer.py
@@ -0,0 +1,26 @@
+import os
+print("Select your distribution")
+print("1 = Debian/Ubuntu")
+print("2 = Arch")
+scleta = input("Write and click enter: ")
+if scelta == 1:
+    os.system("sudo apt install flameshot tesseract-ocr tesseract-ocr-eng python3-pip")
+    s.system("pip install pillow pyautogui pytesseract flask")
+    
+
+elif scelta == 2:
+    os.system("sudo pacman -S flameshot tesseract-ocr tesseract-data-eng python3-pip")
+    s.system("pip install pillow pyautogui pytesseract flask --break-system-package")
+    
+else:
+    print("Not an option")
+    os.system("sleep 3")
+    exit()
+
+
+
+os.system('flameshot config -f "screen"')
+os.system("clear")
+print("Now you must turn of notification in this pannel")
+print("sleep 3")
+os.system("flameshot config")
