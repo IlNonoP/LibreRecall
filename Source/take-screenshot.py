11861baa6a142a717d4f1ca7840dff0e342230d2
IlNonoP <IlNonoP@outlook.it>
2024-06-21 11:42:17 +0200

    Update take-screenshot.py

--- Source/take-screenshot.py
+++ Source/take-screenshot.py
@@ -8,27 +8,27 @@ from datetime import datetime
 os.makedirs(os.path.dirname("static/images/"), exist_ok=True)
 
 
-while True:
+  
+os.system('while xset q | grep -q "Monitor is Off"; do sleep 1; done') #solo se è sbloccato
+
     # Esegui lo screenshot 
-    os.system("flameshot full --path static/images --delay 0")       
+os.system("flameshot full --path static/images --delay 0")       
    
 
 
     # OCR dell'immagine catturata
-    image_path = 'static/images/screen.png'
-    ocr_text = pytesseract.image_to_string(Image.open(image_path))
+image_path = 'static/images/screen.png'
+ocr_text = pytesseract.image_to_string(Image.open(image_path))
     # print(ocr_text)
 
     # Genera un nome univoco per i file
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    name_png = f"{timestamp}.png"
-    name_txt = f"{timestamp}.txt"
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+name_png = f"{timestamp}.png"
+name_txt = f"{timestamp}.txt"
 
     # Rinomina lo screenshot catturato
-    os.system(f"mv {image_path} static/images/{name_png}")
-
+os.system(f"mv {image_path} static/images/{name_png}")
     # Scrivi il testo OCR su un file di testo
-    with open(f"static/images/{name_txt}", 'w') as file:
-        file.write(ocr_text)
+with open(f"static/images/{name_txt}", 'w') as file:
+    file.write(ocr_text)
 
-    os.system("sleep 30")
