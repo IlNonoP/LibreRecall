cab272e7984036e4901b33e0d6f0b41f91f4bd2b
IlNonoP <IlNonoP@outlook.it>
2024-08-29 11:38:12 +0200

    Update main.py

--- Source/main.py
+++ Source/main.py
@@ -2,11 +2,22 @@ import subprocess
 import time
 import os
 
+#def data from config file
+file_config = open('LibreRecall.conf', 'r') 
+data_config = file_config.readlines() 
+sleep_time = data_config[3].split('=', 1)[1]
+sleep_time = sleep_time.split(' ', 1)[0]
+
+server_mode = data_config[5].split('=', 1)[1]
+server_mode = server_mode.split(' ', 1)[0]
+
+
 
 def take_screenshot():
     while True:    
         os.system("python3 take-screenshot.py")
-        os.system("sleep 30")
+        comando = "sleep "+ sleep_time
+        os.system(comando)
    
 
 def run_app():
@@ -14,5 +25,8 @@ def run_app():
 
 if __name__ == "__main__":
     run_app()
-    take_screenshot()       
+    if server_mode == "true":
+        print("Server mode enable")   
+    else:
+        take_screenshot()  
           
