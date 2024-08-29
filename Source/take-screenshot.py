6d0556c4c2010055a28c55a539d19bf51cd8b732
IlNonoP <IlNonoP@outlook.it>
2024-08-29 11:38:26 +0200

    Update take-screenshot.py

--- Source/take-screenshot.py
+++ Source/take-screenshot.py
@@ -3,21 +3,28 @@ from PIL import Image
 import pytesseract
 from datetime import datetime
 
+#set data from LibreRecall.conf
+file_config = open('LibreRecall.conf', 'r') 
+data_config = file_config.readlines() 
+image_directory = data_config[2].split('"', 1)[1]
+image_directory = image_directory.split('"', 1)[0]
+print(image_directory)
 
 
-os.makedirs(os.path.dirname("static/images/"), exist_ok=True)
+os.makedirs(os.path.dirname(image_directory), exist_ok=True)
 
 
   
 os.system('while xset q | grep -q "Monitor is Off"; do sleep 1; done') #solo se è sbloccato
 
-    # Esegui lo screenshot 
-os.system("flameshot full --path static/images --delay 0")       
+    # Esegui lo screenshot
+comando = "flameshot full --path {} --delay 0".format(image_directory)
+os.system(comando)       
    
 
 
     # OCR dell'immagine catturata
-image_path = 'static/images/screen.png'
+image_path = '{}/screen.png'.format(image_directory)
 ocr_text = pytesseract.image_to_string(Image.open(image_path))
 ocr_text = ocr_text.lower()
     # print(ocr_text)
@@ -28,8 +35,8 @@ name_png = f"{timestamp}.png"
 name_txt = f"{timestamp}.txt"
 
     # Rinomina lo screenshot catturato
-os.system(f"mv {image_path} static/images/{name_png}")
+os.system(f"mv {image_path} {image_directory}/{name_png}")
     # Scrivi il testo OCR su un file di testo
-with open(f"static/images/{name_txt}", 'w') as file:
+with open(f"{image_directory}{name_txt}", 'w') as file:
     file.write(ocr_text)
 
